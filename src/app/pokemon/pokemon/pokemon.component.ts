eed628a2e68da200a7bc3a2f998a2a7eb9d11ece
EgorKirillov <80296253508@mail.ru>
2022-11-19 23:22:53 +0300

    fix: variant

--- src/app/pokemon/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon/pokemon.component.ts
@@ -19,6 +19,7 @@ export class PokemonComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     console.log(this.router)
+    // this.id = this.router.snapshot.data['id'] //
     this.routeSub=this.router.params.subscribe(params=> {
       this.id = params['id']
     })
