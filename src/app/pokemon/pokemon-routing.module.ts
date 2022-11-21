9bff4188d28518895d2ba3332ad91ab3f6d3e583
EgorKirillov <80296253508@mail.ru>
2022-11-21 17:56:52 +0300

    refactoring: change routing

--- src/app/pokemon/pokemon-routing.module.ts
+++ src/app/pokemon/pokemon-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonComponent } from "./pokemon/pokemon.component";
 
-const routes: Routes = [{  path: ':id',  component: PokemonComponent,},];
+const routes: Routes = [{  path: 'pokemons/:id',  component: PokemonComponent,},];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pokemons/pokemons-routing.module.ts
+++ src/app/pokemons/pokemons-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonsComponent } from './pokemons/pokemons.component';
 
-const routes: Routes = [{  path: '',  component: PokemonsComponent, pathMatch: 'full'},];
+const routes: Routes = [
+  // {  path: '',  component: PokemonsComponent, pathMatch: 'full'},
+  {path: 'pokemons', component: PokemonsComponent, pathMatch: 'full'},
+  {path: '', redirectTo: 'pokemons', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PokemonsRoutingModule { }
+export class PokemonsRoutingModule {
+}
